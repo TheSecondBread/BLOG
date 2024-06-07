fbd2a5e1351228d175dcc96135a8c3336a7a89cd
TheSecondBread <gamezone2904@gmail.com>
2024-06-07 21:34:10 +0530

    analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Blogs from "./components/Blogs";
 import Nav from "./components/Nav";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+    <Analytics></Analytics>
       <Nav />
       <center>
         <Blogs></Blogs>
